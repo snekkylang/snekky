be068786b8f2ce6c381530dda19baf185b7483f0
Senk Ju <senkju4@gmail.com>
2020-08-20 19:25:04 +0200

    add explicit return types

--- src/lexer/Helper.hx
+++ src/lexer/Helper.hx
@@ -2,15 +2,15 @@ package lexer;
 
 class Helper {
     
-    public static function isLinebreak(s:String) {
+    public static function isLinebreak(s:String):Bool {
         return ~/\r\n|\r|\n/.match(s);
     }
 
-    public static function isAscii(s:String) {
+    public static function isAscii(s:String):Bool {
         return ~/^[a-zA-Z0-9_]*$/.match(s);
     }
 
-    public static function isNumber(s:String) {
+    public static function isNumber(s:String):Bool {
         return ~/^[0-9]+$/.match(s);
     }
 }
