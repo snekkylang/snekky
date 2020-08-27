087409264bd369aa18b884949d899116dc1efba1
Senk Ju <senkju4@gmail.com>
2020-08-27 16:30:05 +0200

    remove symboltable from evaluator

--- src/Snekky.hx
+++ src/Snekky.hx
@@ -11,13 +11,14 @@ class Snekky {
     public static function main() {
         final code = File.getContent("./input.snek");
 
+        var cTime = Sys.time();
         final lexer = new Lexer(code);
         final parser = new Parser(lexer);
         parser.generateAst();
         //parser.writeAst();
         final compiler = new Compiler();
         compiler.compile(parser.ast);
-        final evaluator = new Evaluator(compiler.instructions.getBytes(), compiler.constants, compiler.symbolTable);
+        final evaluator = new Evaluator(compiler.instructions.getBytes(), compiler.constants);
         evaluator.eval();
     }
 }
