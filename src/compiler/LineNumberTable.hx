67fa2fd11845fa8c528ea5a5074dda228ae22959
Senk Ju <senkju4@gmail.com>
2020-09-02 19:15:00 +0200

    re-add basic support for stack traces

---
+++ src/compiler/LineNumberTable.hx
@@ -0,0 +1,16 @@
+package compiler;
+
+class LineNumberTable {
+
+    final table:Map<Int, {line:Int, linePos:Int}> = new Map();
+
+    public function new() {}
+
+    public function define(byteIndex:Int, sourcePosition:{line:Int, linePos:Int}) {
+        table.set(byteIndex, sourcePosition);
+    }
+
+    public function resolve(byteIndex:Int) {
+        return table.get(byteIndex);
+    }
+}
