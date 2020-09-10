e95c16a00de943df77f648b0470222161e5a3676
Senk Ju <senkju4@gmail.com>
2020-09-10 17:11:34 +0200

    remove enum extract function from objecthelper

--- src/object/ObjectHelper.hx
+++ src/object/ObjectHelper.hx
@@ -17,16 +17,4 @@ class ObjectHelper {
 
         return clone;
     }
-
-    public static macro function extract(value:ExprOf<EnumValue>, pattern:Expr):Expr {
-        switch (pattern) {
-            case macro $a => $b:
-                return macro switch ($value) {
-                    case $a: $b;
-                    default: throw "no match";
-                }
-            default:
-                throw new Error("Invalid enum value extraction pattern", pattern.pos);
-        }
-    }
 }
