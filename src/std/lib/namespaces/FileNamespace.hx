075ad3f5e6a77edc9cf397e09ae208b25fb7fcf5
Senk Ju <senkju4@gmail.com>
2020-09-24 21:03:42 +0200

    rename File.get_content and add write function

--- src/std/lib/namespaces/FileNamespace.hx
+++ src/std/lib/namespaces/FileNamespace.hx
@@ -11,12 +11,30 @@ class FileNamespace extends Namespace {
     public function new(evaluator:Evaluator) {
         super(evaluator);
 
-        addFunctionMember("get_content", 1, function(parameters) {
-            switch (parameters[0]) {
-                case Object.String(path):
-                    final content = File.getContent(path);
-                    return Object.String(content);
-                default: error("failed to open file");
+        addFunctionMember("read_utf8", 1, function(parameters) {
+            try {
+                switch (parameters[0]) {
+                    case Object.String(path):
+                        final content = File.getContent(path);
+                        return Object.String(content);
+                    default: error('expected String, got ${parameters[0].getName()}');
+                }
+            } catch (e) {
+                error("failed to open file");
+            }
+
+            return Object.Null;
+        });
+
+        addFunctionMember("write_utf8", 2, function(parameters) {
+            try {
+                switch [parameters[0], parameters[1]] {
+                    case [Object.String(path), Object.String(value)]:
+                        File.saveContent(path, value);
+                    default: 'expected String, got ${parameters[0].getName()}';
+                }
+            } catch (e) {
+                error("failed to open file");
             }
 
             return Object.Null;
