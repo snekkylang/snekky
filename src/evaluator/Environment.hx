252f10b674a94b5f13c198f9c547f7538c837194
Senk Ju <senkju4@gmail.com>
2020-09-29 22:14:01 +0200

    fix issue with parameters in recursive calls

--- src/evaluator/Environment.hx
+++ src/evaluator/Environment.hx
@@ -4,17 +4,33 @@ import object.Object;
 
 class Environment {
 
-    final variables:Array<Object> = [];
+    final variables:Array<Array<Object>> = [];
+    public var depth = 0;
 
-    public function new() {
-
-    }
+    public function new() {}
 
     public function setVariable(index:Int, value:Object) {
-        variables[index] = value;
+        if (variables[index] == null) {
+            variables[index] = [];
+            variables[index][depth] = value;
+        } else if (variables[index][0] != null) {
+            variables[index][0] = value;
+        } else {
+            variables[index][depth] = value;
+        }
     }
 
     public function getVariable(index:Int):Object {
-        return variables[index];
+        var i = depth;
+
+        while (i >= 0) {
+            if (variables[index][i] != null) {
+                return variables[index][i];
+            }
+
+            i--;
+        }
+
+        return null;
     }
 }
