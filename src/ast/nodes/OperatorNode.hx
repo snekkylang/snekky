54cf5c9f5a371368d9e0f90f2e7eafaa9fe5d9a1
Senk Ju <senkju4@gmail.com>
2020-09-30 18:09:02 +0200

    add access information to variable class fields

--- src/ast/nodes/OperatorNode.hx
+++ src/ast/nodes/OperatorNode.hx
@@ -2,8 +2,8 @@ package ast.nodes;
 
 class OperatorNode extends Node {
     
-    public var left:Node;
-    public var right:Node;
+    public final left:Node;
+    public final right:Node;
 
     public function new(position:Int, type:NodeType, left:Node, right:Node) {
         super(position, type);
