6b6f7595542688dc7323175ecd44939e03519836
Senk Ju <senkju4@gmail.com>
2020-10-03 00:45:23 +0200

    improve code of env.setVariable method

--- src/evaluator/Environment.hx
+++ src/evaluator/Environment.hx
@@ -12,12 +12,9 @@ class Environment {
     public function setVariable(index:Int, value:Object) {
         if (variables[index] == null) {
             variables[index] = [];
-            variables[index][depth] = value;
-        } else if (variables[index][0] != null) {
-            variables[index][0] = value;
-        } else {
-            variables[index][depth] = value;
         }
+            
+        variables[index][depth] = value;
     }
 
     public function getVariable(index:Int):Object {
