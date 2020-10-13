685e9338d2ee4d5d003cbd3286d60cd6a2bf4e78
Senk Ju <senkju4@gmail.com>
2020-10-14 00:02:05 +0200

    inline token helper functions

--- src/lexer/Helper.hx
+++ src/lexer/Helper.hx
@@ -2,15 +2,15 @@ package lexer;
 
 class Helper {
     
-    public static function isLinebreak(s:String):Bool {
+    public static inline function isLinebreak(s:String):Bool {
         return ~/\r\n|\r|\n/.match(s);
     }
 
-    public static function isAscii(s:String):Bool {
+    public static inline function isAscii(s:String):Bool {
         return ~/^[a-zA-Z0-9_]+$/.match(s);
     }
 
-    public static function isNumber(s:String):Bool {
+    public static inline function isNumber(s:String):Bool {
         return ~/^[0-9]+$/.match(s);
     }
 }
