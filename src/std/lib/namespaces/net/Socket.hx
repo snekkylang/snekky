2e39d4e0005e05673223a0e4281ef7c029938bd3
Senk Ju <18741573+SenkJu@users.noreply.github.com>
2020-12-02 21:01:42 +0100

    improve hex related methods

--- src/std/lib/namespaces/net/Socket.hx
+++ src/std/lib/namespaces/net/Socket.hx
@@ -16,34 +16,38 @@ class Socket extends MemberObject {
 
         final socket = secure ? new sys.ssl.Socket() : new sys.net.Socket();
 
-        addFunctionMember("write", 2, function(p) {
+        addFunctionMember("write", 1, function(p) {
             assertParameterType(p[0], ObjectType.String);
             final msg = cast(p[0], StringObj).value;
-
-            assertParameterType(p[1], ObjectType.Boolean);
-            final hex = cast(p[1], BooleanObj).value;
             
-            if (hex) {
-                socket.output.write(Bytes.ofHex(msg));
-            } else {
-                socket.write(msg);
-            }
+            socket.write(msg);
 
             return new NullObj(evaluator);
         });
 
-        addFunctionMember("read", 2, function(p) {
+        addFunctionMember("writeHex", 1, function(p) {
+            assertParameterType(p[0], ObjectType.String);
+            final msg = cast(p[0], StringObj).value;
+            
+            socket.output.write(Bytes.ofHex(msg));
+
+            return new NullObj(evaluator);
+        });
+
+        addFunctionMember("read", 1, function(p) {
             assertParameterType(p[0], ObjectType.Number);
             final length = Std.int(cast(p[0], NumberObj).value);
 
-            assertParameterType(p[1], ObjectType.Boolean);
-            final hex = cast(p[1], BooleanObj).value;
+            final msg = socket.input.read(length).toString();
 
-            final msg = if (hex) {
-                socket.input.read(length).toHex();
-            } else {
-                socket.input.read(length).toString();
-            }
+            return new StringObj(msg, evaluator);
+        });
+
+        addFunctionMember("readHex", 1, function(p) {
+            assertParameterType(p[0], ObjectType.Number);
+            final length = Std.int(cast(p[0], NumberObj).value);
+
+            final msg = socket.input.read(length).toHex();
 
             return new StringObj(msg, evaluator);
         });
