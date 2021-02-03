f5591fe5eb70fe4578b06fbd96ec6b1d56d74436
Senk Ju <18741573+SenkJu@users.noreply.github.com>
2021-02-03 19:02:27 +0100

    update socket namespace

--- src/std/lib/namespaces/net/Socket.hx
+++ src/std/lib/namespaces/net/Socket.hx
@@ -1,20 +1,18 @@
 package std.lib.namespaces.net;
 
 import haxe.io.Bytes;
-import object.BooleanObj;
 import object.NumberObj;
 import sys.net.Host;
 import object.Object.ObjectType;
 import object.StringObj;
 import object.NullObj;
 import vm.VirtualMachine;
+import sys.net.Socket as SysSocket;
 
 class Socket extends MemberObject {
 
-    public function new(vm:VirtualMachine, host:String, port:Int, secure:Bool) {
+    public function new(vm:VirtualMachine, socket:SysSocket) {
         super(vm);
-
-        final socket = secure ? new sys.ssl.Socket() : new sys.net.Socket();
 
         addFunctionMember("write", [ObjectType.String], function(p) {
             final msg = cast(p[0], StringObj).value;
@@ -37,22 +35,35 @@ class Socket extends MemberObject {
 
             final msg = socket.input.read(length).toString();
 
-            return new StringObj(msg, vm);
+            return try {
+                new StringObj(msg, vm);
+            } catch (err) {
+                new NullObj(vm);
+            }
         });
 
         addFunctionMember("readHex", [ObjectType.Number], function(p) {
             final length = Std.int(cast(p[0], NumberObj).value);
 
-            final msg = socket.input.read(length).toHex();
-
-            return new StringObj(msg, vm);
+            return try {
+                new StringObj(socket.input.read(length).toHex(), vm);
+            } catch (err) {
+                new NullObj(vm);
+            }
         });
 
         addFunctionMember("readLine", [], function(p) {
-            return new StringObj(socket.input.readLine(), vm);
+            return try {
+                new StringObj(socket.input.readLine(), vm);
+            } catch (err) {
+                new NullObj(vm);
+            };
         });
 
-        addFunctionMember("connect", [], function(p) {
+        addFunctionMember("connect", [ObjectType.String, ObjectType.Number], function(p) {
+            final host = cast(p[0], StringObj).value;
+            final port = Std.int(cast(p[1], NumberObj).value);
+
             socket.connect(new Host(host), port);
             socket.setBlocking(true);
 
@@ -61,6 +72,27 @@ class Socket extends MemberObject {
 
         addFunctionMember("close", [], function(p) {
             socket.close();
+
+            return new NullObj(vm);
+        });
+
+        addFunctionMember("accept", [], function(p) {
+            return new Socket(vm, socket.accept()).getMembers();
+        });
+
+        addFunctionMember("bind", [ObjectType.String, ObjectType.Number], function(p) {
+            final host = cast(p[0], StringObj).value;
+            final port = Std.int(cast(p[1], NumberObj).value);
+
+            socket.bind(new Host(host), port);
+
+            return new NullObj(vm);
+        });
+
+        addFunctionMember("listen", [ObjectType.Number], function(p) {
+            final connections = Std.int(cast(p[0], NumberObj).value);
+
+            socket.listen(connections);
 
             return new NullObj(vm);
         });
